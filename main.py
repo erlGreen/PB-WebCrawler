2384905959ab07354949021287fff1f415e887a9
erlGreen <wasiukiewicz98@gmail.com>
2021-11-13 16:57:09 +0100

    single-thread working web-crawler. Crawling based on depth, Graph built

--- main.py
+++ main.py
@@ -1,131 +1,123 @@
 from urllib.request import Request, urlopen, URLError
-from urllib.parse import urljoin, urlparse
+from urllib.parse import urljoin, urlparse, urlsplit, quote
 from bs4 import BeautifulSoup
-import time
-import threading
+from igraph import *
 import queue
 import ssl
+import re
+from django.utils.encoding import iri_to_uri
+import matplotlib.pyplot as plt
 
-class Crawler(threading.Thread):
-    def __init__(self,base_url, links_to_crawl,have_visited, error_links,url_lock):
-       
-        threading.Thread.__init__(self)
-        print(f"Web Crawler worker {threading.current_thread()} has Started")
-        self.base_url = base_url
-        self.links_to_crawl = links_to_crawl
-        self.have_visited = have_visited
-        self.error_links = error_links
-        self.url_lock = url_lock
-    
-    def run(self):
 
-        my_ssl = ssl.create_default_context()
+class Crawler():
+    def __init__(self, base_url, max_depth, graph : Graph):
+
+        self.graph = graph
+
+        # znaki nie-ascii w url
+        if not len(base_url) == len(base_url.encode()):
+            base_url = iri_to_uri(base_url)
+
+        self.graph.add_vertex(base_url, color='blue', label=base_url)
+
+        self.links_to_crawl = queue.Queue()
+        self.links_to_crawl.put(base_url)
+
+        self.marked_pages = {}
+        self.marked_pages[base_url] = 0
+
+        self.max_depth = max_depth
+
+        self.my_ssl = ssl.create_default_context()
 
         # by default when creating a default ssl context and making an handshake
         # we verify the hostname with the certificate but our objective is to crawl
         # the webpage so we will not be checking the validity of the cerfificate.
-        my_ssl.check_hostname = False
+        self.my_ssl.check_hostname = False
 
         # in this case we are not verifying the certificate and any 
         # certificate is accepted in this mode.
-        my_ssl.verify_mode = ssl.CERT_NONE
+        self.my_ssl.verify_mode = ssl.CERT_NONE
 
-        # we are defining an infinite while loop so that all the links in our
-        # queue are processed.
+    def run(self):
 
         while True:
 
-            # In this part of the code we create a global lock on our queue of 
-            # links so that no two threads can access the queue at same time
-            self.url_lock.acquire()
             print(f"Queue Size: {self.links_to_crawl.qsize()}")
-            link = self.links_to_crawl.get()
-            self.url_lock.release()
-
-            # if the link is None the queue is exhausted or the threads are yet
-            # process the links.
-
-            if link is None:
-                break
-            
-            # if The link is already visited we break the execution.
-            if link in self.have_visited:
-                print(f"The link {link} is already visited")
-                break
 
             try:
-                # This method constructs a full "absolute" URL by combining the
-                # base url with other url. this uses components of the base URL, 
-                # in particular the addressing scheme, the network 
-                # location and  the path, to provide missing components 
-                # in the relative URL.
-                # in short we repair our relative url if it is broken.
-                link = urljoin(self.base_url,link)
+                link = self.links_to_crawl.get_nowait()
+            except:
+                link = None
 
-                # we use the header parameter to "spoof" the "User-Agent" header
-                # value which is used by the browser to identify itself. This is
-                # because some servers will only allow the connection if  it comes
-                # from a verified browser. In this case we are using FireFox header. 
+            if link is None:
+                print(f"Total Number of pages visited are {len(self.marked_pages)}")
+                return
+
+            try:
                 req = Request(link, headers= {'User-Agent': 'Mozilla/5.0'})
+                response = urlopen(req, context = self.my_ssl)
 
-                # we are opening the url using a ssl handshake.
-                response = urlopen(req, context=my_ssl)
 
-                print(f"The URL {response.geturl()} crawled with \
-                      status {response.getcode()}")
-
-                # this returns the html representation of the webpage
                 soup = BeautifulSoup(response.read(),"html.parser")
 
-                # in this case we are finding all the links in the page.
                 for a_tag in soup.find_all('a'):
-                    # we are checking of the link is already visited and (network location part) is our
-                    # base url itself.
-                    if (a_tag.get("href") not in self.have_visited) and (urlparse(link).netloc == self.base_url):
-                        self.links_to_crawl.put(a_tag.get("href"))
-                    
-                    else:
-                        print(f"The link {a_tag.get('href')} is already visited or is not part of the website")
+                    child_link = a_tag.get("href")
 
-                print(f"Adding {link} to the crawled list")
-                self.have_visited.add(link)
+                    child_link = urljoin(link,child_link)
+
+                    child_link_marked = child_link in self.marked_pages
+
+                    if child_link_marked:
+                        # sprawdzenie czy aktualne dojście do strony jest szybsze niż zapisane - jeżeli tak to trzeba ją jeszcze raz sprawdzić ze zmienioną wagą depth
+                        current_depth = self.marked_pages[link]
+                        current_child_depth = self.marked_pages[child_link]
+                        child_depth = min(current_depth + 1, current_child_depth)
+
+                        if child_depth < current_child_depth:
+                            child_needs_revisit = True
+                        else:
+                            child_needs_revisit = False
+                    else:
+                        self.graph.add_vertex(child_link, label=child_link)
+                        child_depth = self.marked_pages[link] + 1
+
+                    # dodaj krawędź tylko jeżeli jej jeszcze nie ma
+                    if not (self.graph.vs.find(name=child_link) in self.graph.vs.find(name=link).neighbors()):
+                        self.graph.add_edge(link, child_link)
+
+                    if ((not child_link_marked) and child_depth <= self.max_depth) or (child_link_marked and child_needs_revisit):
+                        self.links_to_crawl.put(child_link)
+
+                        if not len(child_link) == len(child_link.encode()):
+                            child_link = iri_to_uri(child_link)
+                        self.marked_pages[child_link] = child_depth
+
+                print(f"{link} done")
 
             except URLError as e:
-                print(f"URL {link} threw this error {e.reason} while trying to parse")
-
-                self.error_links.append(link)
-
+                print(f"ERROR {link} - {e.reason}")
             finally:
                 self.links_to_crawl.task_done()
 
 if __name__ == '__main__':
-    print("The Crawler is started")
-    base_url = input("Please Enter Website to Crawl > ")
-    number_of_threads = input("Please Enter number of Threads > ")
 
-    links_to_crawl = queue.Queue()
-    url_lock = threading.Lock()
-    links_to_crawl.put(base_url)
+    print("The crawler is started")
 
-    have_visited = set()
-    crawler_threads = []
-    error_links = []
-    #base_url, links_to_crawl,have_visited, error_links,url_lock
-    for i in range(int(number_of_threads)):
-        crawler = Crawler(base_url = base_url, 
-                          links_to_crawl= links_to_crawl, 
-                          have_visited = have_visited,
-                          error_links = error_links,
-                          url_lock = url_lock)
-        
-        crawler.start()
-        crawler_threads.append(crawler)
+    base_url = input("Please enter website to crawl > ")
+    max_depth = int(input("Please enter max depth > "))
 
+    graph = Graph()
+    crawler = Crawler(base_url = base_url,
+                      max_depth = max_depth,
+                      graph = graph)
 
-    for crawler in crawler_threads:
-        crawler.join()
+    crawler.run()
 
+    # fig, ax = plt.subplots()
 
+    # plot(graph, target=ax)
 
-    print(f"Total Number of pages visited are {len(have_visited)}")
-    print(f"Total Number of Errornous links: {len(error_links)}")
+    # plt.show()
+
+    plot(graph)
